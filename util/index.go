1e0fddd1985753e79c2a8e2461e87fac6440cfaf
swnb <swnblovego@gmail.com>
2020-11-01 01:16:58 +0800

    update

--- util/index.go
+++ util/index.go
@@ -2,21 +2,8 @@ package util
 
 import (
 	"fmt"
-	"reflect"
 )
 
-func Compare(a ...interface{}) string {
-	for i, v := range a {
-		if v == nil {
-			fmt.Println("nil")
-		} else {
-			t := reflect.TypeOf(v)
-			if reflect.Slice == t.Kind() {
-				fmt.Println(i+1, t.Name())
-			} else {
-				fmt.Println(i+1, t.Kind().String(), t.Name())
-			}
-		}
-	}
-	return "fsdfsd"
+func Compare(a ...interface{}) {
+	fmt.Println("v1.1.1")
 }
